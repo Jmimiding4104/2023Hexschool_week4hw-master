b9dd75a010ae23e09a69ed370d276c06fbce2952
Jmimiding4104@gmail.com <Jmimiding4104>
2023-09-04 18:52:11 +0800

    revise

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2023Hexschool_week4hw/',
+  base: '/2023Hexschool_week4hw-master/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
